64aabeb9c15d902dba0794155bf69948873d1b42
miiguelvidalbellido <mbellido13@gmail.com>
2022-10-18 15:55:03 +0200

    Divisores mejorado

---
+++ src/frameworkPE/test.java
@@ -0,0 +1,17 @@
+package frameworkPE;
+import frameworkPE.*;
+public class test {
+
+	public static void main(String[] args) {
+		// APARTADO DE TESTEO
+		
+		String[] a_mostrar = divisores.productoDeNumerosParesImpares();
+		System.out.println(a_mostrar[0]);
+		System.out.println(a_mostrar[1]);
+		System.out.println(a_mostrar[2]);
+		System.out.println(a_mostrar[3]);
+		
+		
+	}
+
+}
